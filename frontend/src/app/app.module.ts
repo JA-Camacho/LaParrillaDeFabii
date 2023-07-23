986e2e3c023aa02803d755a554e444093a465ac9
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-22 20:41:28 -0500

    add components: menu, footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
