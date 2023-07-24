c1707733a3bec22d68c8262b986291628b28f38c
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-24 11:02:46 -0500

    Fixing error with ref: platos

--- backend/models/menuDia.js
+++ backend/models/menuDia.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const menuSchema = new Schema({
     dia: {type: String, required: true},
-    platos: [{ type: Schema.Types.ObjectId, ref: 'Plato' }]
+    platos: [{ type: Schema.Types.ObjectId, ref: 'platos' }]
 });
 
 module.exports = mongoose.model('menu_dia', menuSchema);
