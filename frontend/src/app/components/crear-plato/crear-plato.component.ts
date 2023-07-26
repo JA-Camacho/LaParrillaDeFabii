16f3b968b44b522ea89e88969c778c27322e92f2
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-25 23:07:50 -0500

    Add form to create platos

--- frontend/src/app/components/crear-plato/crear-plato.component.ts
+++ frontend/src/app/components/crear-plato/crear-plato.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { Plato } from 'src/app/models/plato';
+import { PlatoService } from 'src/app/services/plato.service';
 
 @Component({
   selector: 'app-crear-plato',
@@ -7,4 +10,50 @@ import { Component } from '@angular/core';
 })
 export class CrearPlatoComponent {
 
+  nombre: string = '';
+  precio: number = 0.0;
+  descripcion: string = '';
+  tipo: string = 'Escoga el Tipo de Plato';
+  image: any;
+
+  constructor(
+    private platoService: PlatoService, 
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.image = reader.result;
+      };
+
+    }
+  }
+  registrarPlato() {
+    if (this.nombre === '')
+      return alert('El nombre del plato esta vacio');
+    if (this.precio === 0.0)
+      return alert('El precio no ha sido establecido');
+    if (this.descripcion === '')
+      return alert('No se ha proporcionado una descripcion');
+    if (this.tipo === "")
+      return alert('No se ha escogido un tipo de plato');
+    let plato = new Plato(
+      this.nombre,
+      this.precio,
+      this.descripcion,
+      this.tipo
+    )
+    this.platoService.postPlato(plato, this.image).subscribe(
+      res => {
+        console.log(res);
+        this.router.navigate(['/admin']);
+      });
+  }
 }
