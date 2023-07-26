83e37c411a68a12ff3fe97d11ff4cfafeeb493ad
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-25 23:08:03 -0500

    See all platos in a table

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plato } from 'src/app/models/plato';
+import { PlatoService } from 'src/app/services/plato.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  platos: Plato[] = [];
 
+  constructor(private platoService: PlatoService) { }
+
+  ngOnInit() {
+    this.obtenerPlatos();
+  }
+
+  obtenerPlatos() {
+    this.platoService.getPlatos().subscribe((res) => {
+      this.platos = res as Plato[];
+    })
+  }
+  eliminarPlato(id: string) {
+    const confirmacion = window.confirm('¿Estás seguro de que deseas eliminar este plato?');
+    if (confirmacion) {
+      this.platoService.deletePlato(id).subscribe(
+        () => {
+          console.log('Plato eliminado exitosamente');
+          this.obtenerPlatos();
+        },
+        error => {
+          console.error('Error al eliminar el plato:', error);
+        }
+      );
+    }
+  }
 }
